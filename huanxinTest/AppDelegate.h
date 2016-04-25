0fe7d2dcad88922f1c3f5bd7ea08ef725be747f7
applicationHe <apple@test.local>
2016-04-25 15:33:08 +0800

    Initial Commit

---
+++ huanxinTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  huanxinTest
+//
+//  Created by apple on 16/4/25.
+//  Copyright © 2016年 何万牡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ huanxinTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  huanxinTest
+//
+//  Created by apple on 16/4/25.
+//  Copyright © 2016年 何万牡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
